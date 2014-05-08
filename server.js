ac0284c5db779398c76d99ce5a2fef9d15596bc9
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-05-07 19:44:38 -0700

    no JSONify

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('received: %s', message);
     });
-    ws.send(JSON.stringify({message: new Date()});
+    ws.send({message: new Date()});
 });
