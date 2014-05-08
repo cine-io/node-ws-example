f431122f60a0acc70ab9d83ffb472d48e4e67c0e
Thomas Shafer <thomasjshafer@gmail.com>
2014-05-07 21:22:24 -0700

    whitespace

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({port: port})
   , numConnections = 0;
 wss.on('connection', function(ws) {
-    numConnections++;
-    if (numConnections % 100 === 0) {
-      console.log('connected', numConnections, 'clients');
-    }
-    ws.on('message', function(message) {
-        console.log('received: %s bytes', message.length);
-        if (!message.match(/^ping/)) ws.send(message);
-    });
+  numConnections++;
+
+  if (numConnections % 100 === 0) {
+    console.log('connected', numConnections, 'clients');
+  }
+
+  ws.on('message', function(message) {
+      console.log('received: %s bytes', message.length);
+      if (!message.match(/^ping/)) ws.send(message);
+  });
+
   ws.on('close', function() {
     console.log('client disconnected');
   });
