2e027b297f0cc1178462fde16052fdeb53b3115d
Jeffrey Wescott <jeffrey@smile.local>
2014-05-07 19:34:50 -0700

    load testing client

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ wss.on('connection', function(ws) {
     ws.on('message', function(message) {
         console.log('received: %s', message);
     });
-    ws.send('server says HELLO via websockets!');
+    ws.send({message: new Date()});
 });
