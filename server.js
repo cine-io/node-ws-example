3dea41a7e94707ceea54a76f37e07a3cc6f1de53
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2014-05-07 20:49:45 -0700

    better logging

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var port = process.env.PORT || 5000;
 var WebSocketServer = require('ws').Server
   , wss = new WebSocketServer({port: port});
+  , numConnections = 0
 wss.on('connection', function(ws) {
+    console.log('connected', ++numConnections, 'clients');
     ws.on('message', function(message) {
-        console.log('received: %s', message);
+        console.log('received: %s bytes', message.length);
         if (!message.match(/^ping/)) ws.send(message);
     });
 });
